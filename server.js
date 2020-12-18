820be118e8cd189347814e327b95ba20e4b66471
Chris Reed <cr31293@gmail.com>
2020-12-18 18:14:28 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Session to track login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.mongoUser}:${process.env.mongoPass}@cluster0.ftj0e.mongodb.net/${process.env.mongoCollection}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+    app.listen(PORT, function() {
+        console.log(`API server now listening on port ${PORT}`)
+    });
+}).catch(err => {
+    console.log(err);
+});
