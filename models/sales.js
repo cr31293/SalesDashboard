bd91a14fc69e88e7b05c88e9249c9c2a7c8d8fa6
Chris Reed <cr31293@gmail.com>
2020-12-18 18:49:44 -0500

    Sales schema added

---
+++ models/sales.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const saleSchema = new Schema({
+    serialNumber: { type: String, required: true },
+    carMake: { type: String, required: true },
+    carModel: { type: String, required: true },
+    carModelYear: { type: Date, required: true },
+    saleAmount: { type: Number, required: true },
+    sellerUserId: { type: String, required: true },
+    date: { type: Date, required: true }
+});
+
+const Sale = mongoose.model('Sales', saleSchema);
+
+module.exports = Sale;
