7a20d9762c26f65640ef258a1208aee81e4953bc
Chris Reed <cr31293@gmail.com>
2020-12-19 00:56:38 -0500

    created client and cleaned out unnecessary react files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
